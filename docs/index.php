142877a01c1023571dd54fbe40f6e9ce2ec2ab4e
Danica Meier <meier3@stolaf.edu>
2017-10-20 21:55:42 -0500

    php attempt 2

--- docs/index.php
+++ docs/index.php
@@ -1,22 +1,19 @@
 <?php include('header.html'); ?>
-
-<!DOCTYPE>
-
 <html>
 
-<head>
-	<title>Ole Guacomole</title>
-	<meta charset="utf-8">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="description" content="Team page for Ole Guacamole @ HackISU">
-	<link rel="stylesheet" href="main.css">
-	<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
-</head>
+	<head>
+		<title>Ole Guacomole</title>
+		<meta charset="utf-8">
+		<meta http-equiv="X-UA-Compatible" content="IE=edge">
+		<meta name="description" content="Team page for Ole Guacamole @ HackISU">
+		<link rel="stylesheet" href="main.css">
+		<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+	</head>
 
-<body>
-	<h3>Ole Guacamole</h3><br>
-	<h4>@ HackISU 2017</h4>
-</body>
+	<body>
+		<h3>Ole Guacamole</h3><br>
+		<h4>@ HackISU 2017</h4>
+	</body>
 
 	
 </html>
