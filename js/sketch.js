c072845b1cd9f1a430b1f84cb19e86151271584f
RobotBo <harrison.skalski@gmail.com>
2017-10-21 01:49:55 -0500

    grid layout

--- js/sketch.js
+++ js/sketch.js
@@ -1,24 +1,35 @@
+var ppt; //pixels per tile
+var columns;
+var rows;
+var grid;
+
 function setup() {
   // Create the canvas
-  createCanvas(720, 400);
+  createCanvas(720, 720);
+  ppt = 20;
+  columns = floor(width/ppt);
+  rows = floor(height/ppt);
+
+  // 2D array board
+  grid = new Array(columns);
+  for(var i = 0; i < columns; i++){
+    grid[i] = new Array(rows);
+  }
+
+  print("Test Print");
+
+
+}
+
+function draw(){
   background(200);
 
-  // Set colors
-  fill(204, 101, 192, 127);
-  stroke(127, 63, 120);
-
-  // A rectangle
-  rect(40, 120, 120, 40);
-  // An ellipse
-  ellipse(240, 240, 80, 80);
-  // A triangle
-  triangle(300, 100, 320, 100, 310, 80);
-
-  // A design for a simple flower
-  translate(580, 200);
-  noStroke();
-  for (var i = 0; i < 10; i ++) {
-    ellipse(0, 30, 20, 80);
-    rotate(PI/5);
+  for ( var i = 0; i < columns;i++) {
+    for ( var j = 0; j < rows;j++) {
+      if ((grid[i][j] == 1)) fill(0);
+      else fill(255);
+      stroke(0);
+      rect(i*ppt, j*ppt, ppt-1, ppt-1);
+    }
   }
 }
