4a2ce303a9ad96fe595b9044bd7041a195f7ee2f
RobotBo <harrison.skalski@gmail.com>
2017-10-21 09:40:30 -0500

    adding color gen to editor

--- js/sketch.js
+++ js/sketch.js
@@ -2,13 +2,24 @@ var ppt; //pixels per tile
 var columns;
 var rows;
 var grid;
+var w;
+var h;
+var side;
+var selection;//color array
+var colorAmount;
 
 function setup() {
+  w=720;//width
+  h=720;//height
+  side = 240; //Sidebar for selection
+  colorAmount = 20;
+
   // Create the canvas
-  createCanvas(720, 720);
-  ppt = 20;
-  columns = floor(width/ppt);
-  rows = floor(height/ppt);
+  createCanvas(w+side, h);
+
+  ppt = 15;
+  columns = floor(w/ppt);
+  rows = floor(h/ppt);
 
   // 2D array board
   grid = new Array(columns);
@@ -16,14 +27,21 @@ function setup() {
     grid[i] = new Array(rows);
   }
 
-  print("Test Print");
+  for(var i = 0; i < columns; i++){
+    for(var j = 0; j < rows; j++){
+      grid[i][j]=0;
+    }
+  }
 
-
+  selection = new Array(colorAmount);
+  for(var i = 0; i < colorAmount; i++){
+    selection[i] = color(random(255),random(255),random(255));
+  }
 }
 
 function draw(){
   background(200);
-
+  //Selection Grid
   for ( var i = 0; i < columns;i++) {
     for ( var j = 0; j < rows;j++) {
       if ((grid[i][j] == 1)) fill(0);
@@ -32,4 +50,21 @@ function draw(){
       rect(i*ppt, j*ppt, ppt-1, ppt-1);
     }
   }
+
+  //Color choices
+  for(var i = 0; i < colorAmount; i++){
+    fill(selection[i]);
+    stroke(0);
+    rect( w+(.5*side),  ppt*5+i*ppt   , ppt-1 , ppt-1 )
+  }
+}
+
+function mouseDragged(){
+  print("Clicked");
+  print("MouseX: ", mouseX, " MouseY: ", mouseY);
+  x = floor(mouseX/ppt);
+  y = floor(mouseY/ppt);
+  print("gridX: ", x, " gridY: ", y);
+  grid[x][y]=1;
+
 }
