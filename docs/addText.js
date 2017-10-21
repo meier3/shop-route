bb3232ced3ec4d0caafc54957c77d80b5068b65c
dettma1 <32919333+dettma1@users.noreply.github.com>
2017-10-21 13:40:37 -0500

    updated function

--- docs/addText.js
+++ docs/addText.js
@@ -1,10 +1,10 @@
 function addItem(){
 	if(e.keyCode == 13) {
-		var item = document.getElementById('itemInput').value;
-		var fullText = document.getElementById('fullList').value;
+		var item = document.getElementById("itemInput").value;
+		var fullText = document.getElementById("fullList").value;
 		fullText.appendChild(item);
 	}
 	else{
-		document.getElementById('fullList').value = "bread";
+		document.getElementById("fullList").value = "bread";
 	}
 }
