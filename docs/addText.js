27c7a37e7094dae5d681464c17e0f7adb6179a68
dettma1 <32919333+dettma1@users.noreply.github.com>
2017-10-21 13:44:49 -0500

    changing function

--- docs/addText.js
+++ docs/addText.js
@@ -2,7 +2,7 @@ function addItem(){
 	if(e.keyCode == 13) {
 		var item = document.getElementById("itemInput").value;
 		var fullText = document.getElementById("fullList").value;
-		fullText.appendChild(item);
+		fullText += item;
 	}
 	else{
 		document.getElementById("fullList").value = "bread";
